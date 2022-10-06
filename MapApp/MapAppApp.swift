dd77e25e06830c3e40697c79e132e87ee6d13faa
AnanaSsSsSsS <elias.gold@bk.ru>
2022-10-06 22:32:06 +0300

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by macbook on 06.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
